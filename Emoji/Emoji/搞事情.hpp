3163d99d234f34068ce68524b0a3b13eeb6af1d7
孙博 <sunbo@sunbodeMBP.workgroup>
2017-06-17 19:56:31 +0800

    update

---
+++ Emoji/Emoji/搞事情.hpp
@@ -0,0 +1,86 @@
+//
+//  搞事情.hpp
+//  Emoji
+//
+//  Created by 孙博 on 2017/6/17.
+//  Copyright © 2017年 孙博. All rights reserved.
+//
+
+#ifndef ____hpp
+#define ____hpp
+
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <vector>
+
+// define an alias of semicolon
+#define 🎬 ;
+
+//define aliases of parentheses
+#define 😠 (
+#define 😡 )
+
+//define aliases of braces
+#define 😢 {
+#define 😥 }
+
+//define aliases of brackets
+#define 😐 [
+#define 😑 ]
+
+//define an alias of greater-than character and less-than character
+#define 🍏 <
+#define 🍎 >
+#define 🍏🍏 <<
+
+//define alias of operators
+#define 💕 =
+#define 💞 +
+#define 💓 -
+#define 💗 /
+#define 🌱 !=
+
+//define an alias of astterisk and reference(mark of 'and'?)
+#define 😫 *
+#define 😎 &
+
+//using namespace std
+#define 😀 using
+#define 😃 namespace
+#define 😄 std
+
+//keywords
+#define 💖 if
+#define 💘 else
+#define 🈶 while
+#define 🈚️ for
+#define 🐒 return
+#define 🈺 const
+#define 🎨 class
+
+//other definition (I do not wanna write comment anymore)
+#define 😭 int
+#define 😛 endl;
+#define 😮 char
+#define 🤠 main
+#define 😬 void
+#define 👞 .
+#define 👟 push_back
+#define 👒 vector
+#define 🎼 string
+#define 🐠 ,
+#define 🤣 public
+#define 😯 :
+#define 🐶🐶🐶 "dog"
+#define 🐱🐱🐱 "cat"
+#define 😳 "🐶💗🐱➕🐱💗🐶"
+#define 🥂 0;
+#define 🍞 1
+#define 🍣 3;
+
+//Finally, not #define directive
+auto & 😰 = std::cout;
+
+
+#endif /* ____hpp */
